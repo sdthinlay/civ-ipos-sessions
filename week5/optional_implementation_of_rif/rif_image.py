b33b3b75cf4783bad3fa75b8fde4dff187da71fe
rafael <rafael.avigad@nmtafe.wa.edu.au>
2023-08-16 14:27:05 +0800

    cleanup up rif object

--- week5/optional_implementation_of_rif/rif_image.py
+++ week5/optional_implementation_of_rif/rif_image.py
@@ -5,26 +5,27 @@ from PIL import Image
 class RifImage:
     """
     RifImage is a custom image file format for educational purposes.
-    It contains a 9-byte header specifying the image's dimensions followed by 
-    a sequence of 3-byte groups representing RGB values.
+    It has a 9-byte header specifying the image's dimensions followed by 
+    RGB values of the image's pixels.
     """
-    def __init__(self, file: str, is_jpeg: bool = False) -> None:
+    def __init__(self, file_path: str, is_jpeg: bool = False) -> None:
         """
         Initializes the RifImage object.
 
         Args:
-            file (str): The path to the image file.
+            file_path (str): The path to the image file.
             is_jpeg (bool, optional): If the given file is in JPEG format. Defaults to False.
         """  # noqa: E501
         if is_jpeg:
-            self.image_pixels_array = np.array(Image.open(file).convert('RGB'))
+            self._image = Image.open(file_path).convert('RGB')
         else:
-            self.image_pixels_array = RifImage._convert_rif_to_numpy_array(file)
+            array_data = self._load_rif(file_path)
+            self._image = Image.fromarray(array_data)
 
     @staticmethod
-    def _convert_rif_to_numpy_array(rif_file_name: str) -> np.ndarray:
+    def _load_rif(rif_file_name: str) -> np.ndarray:
         """
-        Convert a RIF file to a NumPy array.
+        Loads image data from a RIF file.
 
         Args:
             rif_file_name (str): The path to the RIF file.
@@ -32,31 +33,29 @@ class RifImage:
         Returns:
             np.ndarray: The image represented as a NumPy array.
         """
-        with open(rif_file_name, 'rb') as f:
-            header_w, header_h = f.read(9).decode('utf8').split('x')
-            header_w, header_h = int(header_w), int(header_h)
-            total_bytes = 3 * header_w * header_h
-            byte_data = f.read(total_bytes)
+        with open(rif_file_name, 'rb') as file:
+            header_w, header_h = file.read(9).decode('utf8').split('x')
+            byte_data = file.read(3 * int(header_w) * int(header_h))
+        return np.frombuffer(byte_data, dtype=np.uint8).reshape(int(header_w), int(header_h), 3)
 
-        return np.frombuffer(byte_data, dtype=np.uint8).reshape(header_w, header_h, 3)
-
-    def convert_rif_to_jpg(self, file_name: str) -> None:
+    def save_as_jpeg(self, file_path: str) -> None:
         """
-        Convert the RIF image to JPEG and save it.
+        Save the image as a JPEG.
 
         Args:
-            file_name (str): The path to save the JPEG image.
+            file_path (str): The path to save the JPEG image.
         """
-        Image.fromarray(self.image_pixels_array).save(file_name, format='JPEG')
+        self._image.save(file_path, format='JPEG')
 
-    def save(self, file_name: str) -> None:
+    def save_as_rif(self, file_path: str) -> None:
         """
-        Write the image data to a RIF file.
+        Save the image in RIF format.
 
         Args:
-            file_name (str): The path to save the RIF file.
+            file_path (str): The path to save the RIF file.
         """
-        h, w, _ = self.image_pixels_array.shape
-        with open(file_name, "wb") as f:
-            f.write(f'{h:04}x{w:04}'.encode('utf8'))
-            f.write(bytes(self.image_pixels_array.flatten()))
+        array_data = np.array(self._image)
+        h, w, _ = array_data.shape
+        with open(file_path, "wb") as file:
+            file.write(f'{h:04}x{w:04}'.encode('utf8'))
+            file.write(bytes(array_data.flatten()))
